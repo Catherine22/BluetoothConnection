16615a40a5e427c577d773376b745af191587d00
Catherine <yacatherine19@gmail.com>
2016-07-26 10:51:38 +0800

    refer to bluetoothChat remove log view and insecure socket connection

---
+++ app/src/main/java/tw/com/softworld/bluetoothconnection/MainActivity.java
@@ -0,0 +1,23 @@
+package tw.com.softworld.bluetoothconnection;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothChatFragment fragment = new BluetoothChatFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
+    }
+}
